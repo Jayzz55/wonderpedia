21d631bdddcd6d04c0932b3f0713202e0d8796dc
Jayzz55 <vagrant@rails-dev-box>
2014-11-05 06:04:26 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wonderpedia::Application.config.secret_key_base = 'f79d3f0122546b91a6c55ba563ec9f888dddde882c43065852af0bfd7bca42270bce4c9753f94ff1b9dc6907a6ee7bad91d21207033abb604abeae34d57ec607'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wonderpedia::Application.config.session_store :cookie_store, key: '_wonderpedia_session'
