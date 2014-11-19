ed9f2949479751ec1b32b39a1f11bb73e8769e4b
Jayzz55 <jayzzwijono@yahoo.com>
2014-11-19 11:50:33 +0000

    add assets precompile for production mode

---
+++ config/initializers/assets.rb
@@ -0,0 +1,9 @@
+Rails.application.config.assets.precompile += [
+  "epiceditor.js",
+  "base/epiceditor.css",
+  "editor/epic-dark.css",
+  "editor/epic-light.css",
+  "preview/bartik.css",
+  "preview/github.css",
+  "preview-dark.css"
+]
